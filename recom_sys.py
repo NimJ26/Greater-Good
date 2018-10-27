8eb183c875b3091169b5a39975aeba05a2940430
Nim <nctnim@gmail.com>
2018-10-27 21:33:08 +0530

    Recommendation System

---
+++ recom_sys.py
@@ -0,0 +1,44 @@
+# recom_sys
+# installed lightfm as : conda install -c conda-forge lightfm
+import numpy as np
+from lightfm.datasets import fetch_movielens
+from lightfm import LightFM
+
+# fetch data and format import
+data = fetch_movielens(min_rating = 4.0) 
+print(repr(data['train']))
+print(repr(data['test']))
+
+#create model
+model = LightFM(loss = 'warp')
+model.fit(data['train'], epochs = 30, num_threads=2)
+def sample_recommendations(model, data, user_ids):
+	# no. of users and movies in training data
+	n_users, n_items = data['train'].shape
+	#generate recommendation for each user we input
+	for user_id in user_ids:
+		known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
+		# movies our model predicts they will like 
+		scores = model.predict(user_id, np.arange(n_items))
+		#rank them in order of most liked to least
+		top_items = data['item_labels'][np.argsort(-scores)]
+		#print out the results
+		print("User %s" % user_id)
+		print("  Known positives:")
+
+		for x in known_positives[:3]:
+			print("   %s" % x)
+
+		print("   Recommended:")
+
+		for x in top_items[:3]:
+			print("  %s" % x)
+
+
+sample_recommendations(model, data, [3, 25, 450])
+
+
+
+
+
+
